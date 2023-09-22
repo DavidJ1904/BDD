0691d8c7124d951514d34d6b054bbaaeb90beb5b
David1904 <djmax1320@hotmail.com>
2023-09-21 19:15:17 -0500

    Se realizaron los ejercicios

--- scripts/Script 3.sql
+++ scripts/Script 3.sql
@@ -8,28 +8,10 @@ create table estudiante(
 	fecha_nacimiento date,
 	constraint estudiante_pk primary key (cedula)
 )
-insert into estudiante (cedula,nombre,apellido,email,fecha_nacimiento)
-values ('1479801231','Arthur','Sanchez','sanchez123@hotmail.com','03-12-2000')
-
-insert into estudiante (cedula,nombre,apellido,email,fecha_nacimiento)
-values ('1794801231','Juan','Fernando','fercho123@hotmail.com','03-02-2003')
-
-insert into estudiante (cedula,nombre,apellido,email,fecha_nacimiento)
-values ('1109182341','Antonio','Lara','jufer12@hotmail.com','09-02-2010')
-
-insert into estudiante (cedula,nombre,apellido,email,fecha_nacimiento)
-values ('1891230182','Lourdes','Luz','lucero120@hotmail.com','17-04-1999')
-
-insert into estudiante (cedula,nombre,apellido,email,fecha_nacimiento)
-values ('1421236112','Maximo','Escalera','famoso2020@hotmail.com','20-01-2018')
-
-insert into estudiante (cedula,nombre,apellido,email,fecha_nacimiento)
-values ('1872190521','Marcos','Gutierres','luciaxmarco@hotmail.com','08-08-2020')
-
-insert into estudiante (cedula,nombre,apellido,email,fecha_nacimiento)
-values ('1190123492','Sancho','Panza','mexico13@hotmail.com','01-02-1978')
-
-insert into estudiante (cedula,nombre,apellido,email,fecha_nacimiento)
-values ('1311234123','Rodrigo','Fernando','fenand123@hotmail.com','01-01-2000')
+select nombre,cedula from estudiante
 
 select * from estudiante
+where cedula like '17%'
+
+select * from estudiante
+where nombre like 'A%'
--- scripts/Script 5.sql
+++ scripts/Script 5.sql
@@ -8,28 +8,12 @@ create table videojuegos(
 	constraint videojuegos_pk primary key (codigo)
 )
 
-insert into videojuegos(codigo,nombre,descripcion,valoracion)
-values('091823','Minecraft','Juego de supervivencia',5.0)
-
-insert into videojuegos(codigo,nombre,descripcion,valoracion)
-values('124324','Resident evil','Juego de accion',5.0)
-
-insert into videojuegos(codigo,nombre,descripcion,valoracion)
-values('091442','Dead Space','Juego de terror',4.2)
-
-insert into videojuegos(codigo,nombre,descripcion,valoracion)
-values('321433','Cyberpunk','Juego de accion',4.3)
-
-insert into videojuegos(codigo,nombre,descripcion,valoracion)
-values('011823','The Forest','Juego de supervivencia',5.0)
-
-insert into videojuegos(codigo,nombre,valoracion)
-values('041823','ROBLOX',4.1)
-
-insert into videojuegos(codigo,nombre,valoracion)
-values('001232','ARK',4.9)
-
-insert into videojuegos(codigo,nombre,valoracion)
-values('09312','FIFA2020',4.7)
+select * from videojuegos
+where nombre like 'C%'
 
 select * from videojuegos
+where valoracion
+between '9' and '10'
+
+select * from videojuegos
+where descripcion is null
